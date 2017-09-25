18ab27aba2c00eeaf6029f0cdafa000bd83894f2
boomerman <tarunpreet96@gmail.com>
2017-09-25 12:33:10 +0530

    created roles

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  enum role: [:student, :guide, :admin, :coordinator]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :student
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
