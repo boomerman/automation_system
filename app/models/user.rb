279a96318998c67f7b59b3a54b29123ef5ba5581
boomerman <tarunpreet96@gmail.com>
2017-09-29 13:09:14 +0530

    login using roll no

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   def set_default_role
     self.role ||= :student
   end
+validates :college_roll, :presence => true, :uniqueness => true
+validates_length_of :college_roll, :is => 6, :wrong_length => "incomplete"
+
+validates_length_of :university_roll_no, :is => 7, :wrong_length => "incomplete"
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
